9a6a8a2e19ff79b72da894a0eb09ccf94e741655
Dima Gubatenko <dimagubatenko@MacBook-Pro-Dima.local>
2021-05-06 21:53:03 +0300

    fixed get / request

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.post('/', function (req, res) {
+app.get('/', function (req, res) {
   console.log(req.body);
   res.end();
 });
