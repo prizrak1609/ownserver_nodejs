81e57bcc047da9dd90445890ffed9a8063f057e8
Dima Gubatenko <dimagubatenko@MacBook-Pro-Dima.local>
2021-05-06 21:51:14 +0300

    change /data to /

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.post('/data', function (req, res) {
+app.post('/', function (req, res) {
   console.log(req.body);
   res.end();
 });
