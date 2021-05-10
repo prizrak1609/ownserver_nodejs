fe4bf8e263105e4d8f0711b24c984bd23c852b74
Dima Gubatenko <dimagubatenko@MacBook-Pro-Dima.local>
2021-05-10 19:42:44 +0300

    updated getIP function

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
-var requestIp = require('request-ip');
-var fs = require('fs');
 var net = require('net');
 
 var settings = {}
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(requestIp.mw())
 
-function getIP() {
-    var ip = settings["raspberry"]
+function getIP(name) {
+    var ip = settings[name]
     if (net.isIPv6(ip))
        ip = "[" + ip + "]"
     return "http://" + ip + ":8080"
@@ -19,18 +16,18 @@ function getIP() {
 
 app.get('/', function (req, res) {
   //res.send("name " + req.query.name + " address " + req.connection.remoteAddress)
-  res.redirect(getIP())
+    res.redirect(getIP(req.query.name))
 });
 
 app.get('/register', function (req, res) {
     var name = req.query.name
-    var ip = req.clientIp
+    var ip = req.query.ip
     settings[name] = ip
     res.send("registered " + name + " " + ip)
 });
 
 app.use(function (req, res, next) {
-    res.status(404).redirect(getIP())
+    res.status(404).redirect(getIP("raspberry"))
 })
 
 app.listen(process.env.PORT || 8080);
