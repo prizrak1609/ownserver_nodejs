e34688d78a4890a4b36a24942a803fe60e58e185
Dima Gubatenko <dimagubatenko@MacBook-Pro-Dima.local>
2021-08-19 00:17:04 +0300

    added ejs

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ var settings = {}
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 app.get('/', function (req, res) {
-	res.sendFile(`${__dirname}/views/index.html`)
+	res.render("index")
 });
 app.listen(process.env.PORT || 8080);
