4e2f32e9fbafbcd81d3aead52b067258abc5fb45
Dima Gubatenko <dimagubatenko@MacBook-Pro-Dima.local>
2021-08-19 00:01:16 +0300

    updated render code

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
 app.get('/', function (req, res) {
-	res.render('index', { title: 'Markdown Example' });
+	res.sendFile('index.html', {
+        root: path.join(__dirname, 'views')
+    })
 });
 app.listen(process.env.PORT || 8080);
