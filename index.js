29d054754a0f86d1f8e96ac991603c7da0724861
Dima Gubatenko <dimagubatenko@MacBook-Pro-Dima.local>
2021-08-19 00:57:54 +0300

    try 9

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ var fs = require('fs');
 var path = require('path');
 var views = path.join(__dirname, 'views')
 
-// app.use(bodyParser.json()); // for parsing application/json
-// 	app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-// 	app.use('/views', express.static('/views'))
+app.use(bodyParser.json()); // for parsing application/json
+	app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+	app.use(express.static('views'))
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(views, "index.html"))
 });
-
-app.use(express.static('views'));
 
 app.listen(process.env.PORT || 8080);
