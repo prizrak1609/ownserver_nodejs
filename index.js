facc7da6c602e6f336d6d04e6d6db6ec12a9950f
Dima Gubatenko <dimagubatenko@MacBook-Pro-Dima.local>
2021-08-19 00:04:24 +0300

    removed render engine

--- index.js
+++ index.js
@@ -8,16 +8,6 @@ var settings = {}
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.engine('html', function(path, options, fn){
-  fs.readFile(path, 'utf8', function(err, str){
-    if (err) return fn(err);
-    fn(null, str);
-  });
-});
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
-
 app.get('/', function (req, res) {
 	res.sendFile('index.html', {
         root: path.join(__dirname, 'views')
